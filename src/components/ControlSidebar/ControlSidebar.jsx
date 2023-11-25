a492bb10f9ecd3ed228377762edc69cf06c42e5e
dimadidyk0 <dimadidyk0@gmail.com>
2023-11-25 20:06:45 +0100

    feat: add antd and some changes

---
+++ src/components/ControlSidebar/ControlSidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './ControlSidebar.module.css';
+
+export default function ControlSidebar() {
+  return (
+    <div className={s.root}>ControlSidebar</div>
+  )
+}
